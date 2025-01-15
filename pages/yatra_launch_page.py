00e9f147141a75d7010fd74a3fb2f9882d92af11
shubhamshekhar <sshekhar8237@gmail.com>
2025-01-15 16:29:10 +0530

    Added changes for xpath and logic

--- pages/yatra_launch_page.py
+++ pages/yatra_launch_page.py
@@ -1,6 +1,8 @@
 import os
 import pdb
 import time
+from datetime import datetime
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.devtools.v85.debugger import pause
 from selenium.webdriver.common.keys import Keys
@@ -21,10 +23,11 @@ class LaunchPage(BaseDriver):
     GOING_TO_FIELD = "//p[text()='Going To']"
     GOING_TO_FIELD_KEY = "//input[@id='input-with-icon-adornment']"
     RESULT_LIST = "//div[@class='MuiBox-root css-134xwrj']/ul/div[1]"
-    SELECT_DATE_FIELD = "//div[@class='position-relative MuiBox-root css-0']/div[@class='MuiBox-root css-0']"
+    SELECT_DATE_FIELD = "//div[@class='css-rd021u']"
     ALL_DATES = "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']"
-    SEARCH_BUTTON = "//input[@value='Search Flights']"
+    SEARCH_BUTTON = "//button[normalize-space()='Search']"
     Month_path= "//span[@class='react-datepicker__current-month']"
+    close = "//div[@class='close']"
 
     def getDepartFromField(self,field):
         return self.wait_until_element_is_clickable(By.XPATH,field)
@@ -34,6 +37,11 @@ class LaunchPage(BaseDriver):
 
     def getFromToResults(self):
         return self.wait_for_presence_of_all_elements(By.XPATH, self.RESULT_LIST)
+
+    def close_popup(self):
+        pdb.set_trace()
+        return self.driver.find_element(By.XPATH,self.close)
+
 
     def getDepatureDateField(self):
         return self.wait_until_element_is_clickable(By.XPATH, self.SELECT_DATE_FIELD)
@@ -85,16 +93,29 @@ class LaunchPage(BaseDriver):
 
     # react-datepicker__day react-datepicker__day--0
     def enterDepartureDate(self, departuredate):
+
         self.getDepatureDateField().click()
-        pdb.set_trace()
-        current_month = self.driver.find_element(By.XPATH,self.Month_path).text
+        current_month = self.driver.find_elements(By.XPATH,self.Month_path)
+        date_obj = datetime.strptime(departuredate, "%d %B %Y")
+        depart_month_year = formatted_date = date_obj.strftime("%B %Y")
+        depart_day_month= formatted_date = date_obj.strftime("%B %d")
 
-        all_dates = self.getAllDatesField().find_elements(By.XPATH, self.ALL_DATES)
-        for date in all_dates:
-            if date.get_attribute("data-date") == departuredate:
-                date.click()
+        for month in current_month:
+            print(month.text)
+            if month.text == depart_month_year:
+                self.log.info("month found in the dates")
                 break
+            else:
+                self.wait_until_element_is_clickable(By.XPATH,"//div[contains(@class,'MuiBox-root css-hm78w')]//div[3]//div[1]//div[1]//div[1]//button[2]").click()
+                current_month += self.driver.find_elements(By.XPATH, self.Month_path)
 
+        day = f"//div[contains(@aria-label,'{depart_day_month}')]"
+        day_element= self.driver.find_element(By.XPATH,day)
+        day_element.click()
+        # for date in all_dates:
+        #     if date.get_attribute("data-date") == departuredate:
+        #         date.click()
+        #         break
     def clickSearchFlightsButton(self):
         self.getSearchButton().click()
         time.sleep(4)
@@ -102,7 +123,8 @@ class LaunchPage(BaseDriver):
     def searchFlights(self, departlocation, goingtolocation, departuredate):
         self.enterDepartFromLocation(departlocation)
         self.enterGoingToLocation(goingtolocation)
+
         self.enterDepartureDate(departuredate)
         self.clickSearchFlightsButton()
-        search_flights_result = SearchFlightResults(self.driver)
-        return search_flights_result
+        # search_flights_result = SearchFlightResults(self.driver)
+        # return search_flights_result
